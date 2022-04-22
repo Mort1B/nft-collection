8ac233749a62859541755a4fdccbd84327787622
Morten Bergseng <bergseng.m@gmail.com>
2022-04-22 14:00:37 +0200

    fix

--- nft/pages/index.js
+++ nft/pages/index.js
@@ -2,7 +2,7 @@ import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import { abi, NFT_CONTRACT_ADDRESS } from "../../hardhat-tut/constants";
+import { abi, NFT_CONTRACT_ADDRESS } from "../constants/index";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
