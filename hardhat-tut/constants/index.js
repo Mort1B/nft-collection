ae1a72519dc7b3e15fc07ce7fe748b99bad92dc3
Morten Bergseng <bergseng.m@gmail.com>
2022-04-22 12:33:00 +0200

    zz

--- hardhat-tut/constants/index.js
+++ hardhat-tut/constants/index.js
@@ -1,6 +1,9 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = 0xB9Ae2e60e95044D4640423073FFDE5F498251bf9;
+const WHITELIST_CONTRACT_ADDRESS = "0xB9Ae2e60e95044D4640423073FFDE5F498251bf9";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+
+/// contract address 0x305F96f7548447BF418Bc951723F32Ee87574c52
